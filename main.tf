b6302e8946b124eabd5fbb0bfd4ea85aa6e9138c
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-02-09 00:10:17 -0600

    Updates deployment process to use kustomize and 1.2 script
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- main.tf
+++ main.tf
@@ -3,30 +3,13 @@ locals {
   bin_dir       = module.setup_clis.bin_dir
   yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/${local.name}"
 
-  layer = "services"
+  layer = "infrastructure"
   application_branch = "main"
   layer_config = var.gitops_config[local.layer]
 }
 
 module setup_clis {
   source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
-}
-
-module "service_account" {
-  source = "github.com/cloud-native-toolkit/terraform-gitops-service-account"
-
-  gitops_config = var.gitops_config
-  git_credentials = var.git_credentials
-  namespace = var.namespace
-  name = "installplan-approver-job"
-  rbac_rules = [{
-    apiGroups = ["operators.coreos.com"]
-    resources = ["installplans","subscriptions"]
-    verbs = ["get","list","patch"]
-  }]
-  sccs = ["anyuid"]
-  server_name = var.server_name
-  rbac_cluster_scope = false
 }
 
 resource null_resource create_yaml {
--- outputs.tf
+++ outputs.tf
@@ -1,5 +1,5 @@
-#output "myoutput" {
-#  description = "Description of my output"
-#  value       = "value"
-#  depends_on  = [<some resource>]
-#}
+output "namespace" {
+  description = "The namespace where cert manager has been deployed"
+  value       = var.namespace
+  depends_on  = [null_resource.setup_gitops]
+}
