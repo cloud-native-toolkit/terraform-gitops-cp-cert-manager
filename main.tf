92131baade88690469bfe903da2aeb2c0c087da3
Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>
2022-02-11 13:05:46 -0600

    Pulls cert-manager yaml directly from hosted repo
    Signed-off-by: Cloud-Native Toolkit <cloudnativetoolkit@gmail.com>

--- main.tf
+++ main.tf
@@ -1,34 +1,19 @@
 locals {
   name          = "certmgr"
-  bin_dir       = module.setup_clis.bin_dir
   yaml_dir      = "${path.cwd}/.tmp/${local.name}/chart/${local.name}"
 
   layer = "infrastructure"
   type = "base"
   application_branch = "main"
   layer_config = var.gitops_config[local.layer]
-}
-
-module setup_clis {
-  source = "github.com/cloud-native-toolkit/terraform-util-clis.git"
-}
-
-resource null_resource create_yaml {
-  provisioner "local-exec" {
-    command = "${path.module}/scripts/create-yaml.sh '${local.yaml_dir}' '${var.namespace}'"
-
-    environment = {
-      BIN_DIR = local.bin_dir
-    }
-  }
+  namespace = "cert-manager"
 }
 
 resource gitops_module module {
-  depends_on = [null_resource.create_yaml]
 
   name        = local.name
-  namespace   = var.namespace
-  content_dir = local.yaml_dir
+  namespace   = local.namespace
+  content_dir = "https://github.com/jetstack/cert-manager/releases/download/v1.2.0/cert-manager.yaml"
   server_name = var.server_name
   layer       = local.layer
   type        = local.type
